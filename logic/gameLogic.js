c98d97bffd0dc032d4f2ff6bbdbc338f33829419
Son Hai Nguyen <sirarseus@gmail.com>
2014-09-08 18:17:25 +0200

    Fix: pokud se dosáhlo high score, tak "high" se ukazovalo pořád nehledě na scóre

--- logic/gameLogic.js
+++ logic/gameLogic.js
@@ -35,6 +35,10 @@ function end() {
         scoreDialog.highScore = true
     }
 
+    else {
+        scoreDialog.highScore = false
+    }
+
     pause()
     rootMouseArea.enabled = false
     game.over = true
